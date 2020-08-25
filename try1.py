802e941e32ced61805d861cdffe64ef65d8b687d
Bob <mauricio.silva9614@gmail.com>
2020-08-24 21:47:49 -0500

    cambio

--- try1.py
+++ try1.py
@@ -25,7 +25,7 @@ else:
     s=smtplib.SMTP("smtp.zoho.com",587)
     msg=MIMEText(total)
     sender="noticiasls@zohomail.com"
-    recipients="Alonsotakamure@hotmail.com"
+    recipients="noticiasls@zohomail.com"
     msg["Subject"]="Noticias"
     msg["From"]=sender
     msg["To"]=recipients
