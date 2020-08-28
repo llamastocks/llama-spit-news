cb8ed3214abfc42918c048c9d86ca081640784a9
Bob <mauricio.silva9614@gmail.com>
2020-08-27 23:41:16 -0500

    secciones

--- chino.py
+++ chino.py
@@ -4,17 +4,19 @@ from bs4 import BeautifulSoup
 from newspaper import Article
 from pymongo import MongoClient
 
-url="https://elcomercio.pe/politica/se-viene-una-campana-una-cronica-de-fernando-vivas-noticia/"
+url="https://elcomercio.pe/politica/congreso-el-fin-de-la-votacion-a-traves-de-portavoces-tras-el-pronunciamiento-del-tc-noticia/"
 
 
 article=Article(url,language="es")
 article.download()
 article.parse()
+article.nlp()
 articulo={
 "Titulo":article.title,
 "Fecha":article.publish_date,
 "URL":article.url,
-"Article":article.text
+"Article":article.text,
+"Keywords":article.keywords
 }
 client=MongoClient("mongodb+srv://root_bobsburguers:yoQnE9BsxD8YqpqL@bobsburguerscluster-z0q0x.mongodb.net/test?retryWrites=true&w=majority")
 db=client.elcomercio_economia
--- news.py
+++ news.py
@@ -5,36 +5,38 @@ from bs4 import BeautifulSoup
 from newspaper import Article
 from pymongo import MongoClient
 
+sections=["economia","politica","mundo"]
+for section in sections:
+    url="https://elcomercio.pe/"+section
+    contenido=requests.get(url, verify=False).text
 
-url="https://elcomercio.pe/economia"
-contenido=requests.get(url, verify=False).text
+    soup=BeautifulSoup(contenido,"html.parser")
+    p=soup.find_all("div",{"class":"story-item__wrapper-item w-full"})
+    links=[]
+    for a in p:
+        for b in a.find_all("a",{"class":"story-item__title block overflow-hidden primary-font line-h-xs mt-10"}):
+            links.append(b.get("href"))
 
-soup=BeautifulSoup(contenido,"html.parser")
-p=soup.find_all("div",{"class":"story-item__wrapper-item w-full"})
-links=[]
-for a in p:
-    for b in a.find_all("a",{"class":"story-item__title block overflow-hidden primary-font line-h-xs mt-10"}):
-        links.append(b.get("href"))
+    for link in links:
+        url="https://elcomercio.pe"+link
+        article=Article(url,language="es")
+        article.download()
+        article.parse()
+        article.nlp()
+        key=", ".join(article.keywords)
 
-for link in links:
-    url="https://elcomercio.pe"+link
-    article=Article(url,language="es")
-    article.download()
-    article.parse()
-    article.nlp()
-    key=", ".join(article.keywords)
-
-    articulo={
-    "Titulo":article.title,
-    "Fecha":article.publish_date,
-    "URL":article.url,
-    "Article":article.text,
-    "Keywords":"Palabras clave: "+key
-    }
-    
-    client=MongoClient("mongodb+srv://root_bobsburguers:yoQnE9BsxD8YqpqL@bobsburguerscluster-z0q0x.mongodb.net/test?retryWrites=true&w=majority")
-    db=client.elcomercio_economia
-    result=db.noticias.update(articulo,articulo,upsert=True)
-    
-print("Se han actualizado las noticias")    
+        articulo={
+        "Titulo":article.title,
+        "Fecha":article.publish_date,
+        "URL":article.url,
+        "Article":article.text,
+        "Keywords":"Palabras clave: "+key,
+        "Section":section
+        }
+        
+        client=MongoClient("mongodb+srv://root_bobsburguers:yoQnE9BsxD8YqpqL@bobsburguerscluster-z0q0x.mongodb.net/test?retryWrites=true&w=majority")
+        db=client.elcomercio_economia
+        result=db.noticias.update(articulo,articulo,upsert=True)
+        
+    print("Se han actualizado las noticias de sección "+section)    
 
