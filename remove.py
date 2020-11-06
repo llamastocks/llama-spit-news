86b5b762038188d62f8fc0c6ba4a4ed5f010d7e2
Bob <mauricio.silva9614@gmail.com>
2020-11-06 00:15:31 -0500

    remove

--- remove.py
+++ remove.py
@@ -1,4 +1,9 @@
 from pymongo import MongoClient
+import datetime
+
+date=datetime.datetime.now()+datetime.timedelta(hours=0,minutes=-180)
+print(date)
 client=MongoClient("mongodb+srv://root_bobsburguers:yoQnE9BsxD8YqpqL@bobsburguerscluster-z0q0x.mongodb.net/test?retryWrites=true&w=majority")
 db=client.llamastocks
-db.news.remove()
+
+db.news.remove({"Fecha":{"$lt":date}})
