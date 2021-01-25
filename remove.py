f9ff2a3840a8f8022b56434bd488196ab7db7206
Bob <mauricio.silva9614@gmail.com>
2021-01-24 22:24:37 -0500

    cambios

--- remove.py
+++ remove.py
@@ -1,9 +1,11 @@
 from pymongo import MongoClient
 import datetime
 
-date=datetime.datetime.now()+datetime.timedelta(hours=0,minutes=-180)
-print(date)
+date_1=datetime.datetime.now()+datetime.timedelta(hours=0,minutes=-180)
+date_2=datetime.datetime.now()
+print(date_1)
+print(date_2)
 client=MongoClient("mongodb+srv://root_bobsburguers:yoQnE9BsxD8YqpqL@bobsburguerscluster-z0q0x.mongodb.net/test?retryWrites=true&w=majority")
 db=client.llamastocks
 
-db.news.remove({"Fecha":{"$lt":date}})
+db.news.remove({"Fecha":{"$lt":date_1,"$gt":date_2}})
